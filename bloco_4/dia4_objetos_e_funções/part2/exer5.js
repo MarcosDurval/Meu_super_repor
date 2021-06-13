545a0ec41f35c3ae5bdeb968d965163c3f5018a0
Marcos Durval <marc0sdurval20@gmail.com>
2021-06-13 14:51:20 -0300

    exer

--- bloco_4/dia4_objetos_e_funções/part2/exer5.js
+++ bloco_4/dia4_objetos_e_funções/part2/exer5.js
@@ -2,11 +2,20 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  Recorrente: "sim"
 };
-info.Recorrente = "sim"
+
 let obj = {
   personagem: "tio patinhas",
   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota:"O último MacPatinhas"
+  nota:"O último MacPatinhas",
+  Recorrente: "sim"
 }
-obj.Recorrente = "sim"
+for(let key in info){
+  if(info[key]===obj[key]&& info[key]==="sim"){
+    console.log("ambos recorrentes")
+  }
+  else{
+  console.log(`${info[key]} e ${obj[key]}`)
+  }
+}
--- bloco_4/dia4_objetos_e_funções/part2/funcao/exer3.js
+++ bloco_4/dia4_objetos_e_funções/part2/funcao/exer3.js
@@ -1,7 +1,8 @@
 // Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 //Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
-let array = [2, 4, 6, 7, 10, 0, -3];
-let maior = 0
+let array = [2, 4, 6, 7, 10, 0, -3,-3,-3];
+let maior = 0;
+let contador = 0;
 let test = 0;
 function valor(){
   for(let position in array){
@@ -13,6 +14,12 @@ function valor(){
       test = position
     }
   }
+  for(index=0;index<array.length;index+=1){
+    if(menor === array[index]){
+      contador+=1
+    }
+  }
+  console.log(contador)
   return test
 }
 console.log(valor())
