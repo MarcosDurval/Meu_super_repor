84a3a02e4ae53c7ad2899f47b1a917bae079c331
marcos <marc0sdurval20@gmail.com>
2021-06-23 14:36:14 -0300

    modificacoes nos arquivos do bloco 7

---
+++ Módulo_1/bloco7-JavaScript-ES6/dia1-let-const/teste.js
@@ -0,0 +1,9 @@
+const createStudent = (entrada) => {
+  if(typeof(entrada) === "string"){
+  return {
+      name: entrada,
+      feedback: feedback = () => 2+2
+    }
+  }
+};
+console.log(createStudent("zero"))
---
+++ Módulo_1/bloco7-JavaScript-ES6/dia3-testes-unitários/exer1.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+const sumTes = sum(4,"5")
+assert.strictEqual(sumTes,1,"error")
