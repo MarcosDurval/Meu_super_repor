17b7410ecb6f2072365fec4d9e9f213a6eb1f001
marcos <marc0sdurval20@gmail.com>
2021-11-10 11:09:45 -0300

    dockerfiletestes

---
+++ Módulo_3/bloco20/dia2/exer2.sql
@@ -0,0 +1,9 @@
+USE Pixar;
+
+SELECT 
+    m.title, b.domestic_sales, b.international_sales
+FROM
+    Movies m
+        INNER JOIN
+    BoxOffice b ON b.movie_id = m.id
+     WHERE b.domestic_sales < b.international_sales;
