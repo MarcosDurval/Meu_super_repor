565f3a902c7a68d091285fef6dab196e8f72bd7f
marcos <marc0sdurval20@gmail.com>
2022-04-21 16:48:56 -0300

    feat: implenta exerc

--- Módulo_3/bloco_30/dia_2/exercicio/src/models/connection.ts
+++ Módulo_3/bloco_30/dia_2/exercicio/src/models/connection.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
-const connectMongo = mongoose.connect('mongodb://localhost:27017/TrybeStore');
+const connectMongo = (mongoDatabaseURI = process.env.MONGO_URI
+  || 'mongodb://localhost:27017/TrybeStore') => 
+  mongoose.connect(mongoDatabaseURI);
 
 export default connectMongo;
--- Módulo_3/bloco_30/dia_2/exercicio/src/service/index.ts
+++ Módulo_3/bloco_30/dia_2/exercicio/src/service/index.ts
@@ -1,21 +1,17 @@
-import { ZodError } from 'zod';
 import Model from '../models';
 
-export interface ServiceError {
-  error: ZodError;
-}
 export default abstract class Service<T> {
   constructor(protected model:Model<T>) { }
 
-  public create = async (obj:T):Promise<T | ServiceError> => 
+  public create = async (obj:T):Promise<T> => 
     this.model.create(obj); 
 
-  public readOne = async (id:string):Promise<T | null | ServiceError > => 
+  public readOne = async (id:string):Promise<T | null > => 
     this.model.readOne(id);
 
-  public read = async (): Promise<T[] | ServiceError > => 
+  public read = async (): Promise<T[] > => 
     this.model.read();
 
-  public delete = async (id:string):Promise<T | null | ServiceError> =>
+  public delete = async (id:string):Promise<T | null > =>
     this.model.delete(id); 
 }
